72dbdc153157742b229f2d798c135fe13d58c88c
ben286 <benwen931@gmail.com>
2019-01-28 21:33:29 +0800

    2小时学会springboot

---
+++ src/main/java/ben286/girl/HelloController.java
@@ -0,0 +1,38 @@
+package ben286.girl;
+
+/*
+Created by Ben Wen on 2019/1/25.
+*/
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@ResponseBody
+//@RestController
+public class HelloController {
+
+//    @Value("${cupSize}")
+//    private String cupSize;
+//
+//    @Value("${age}")
+//    private Integer age;
+//
+//    @Value("${content}")
+//    private String content;
+
+//    @Autowired
+//    private GirlProperties girlProperties;
+
+//    @RequestMapping(value = {"/hello/{id}", "/hi/{id}"}, method = RequestMethod.GET)
+//    @PostMapping("/hi")
+//    @GetMapping("/hello")// GetMapping与PostMapping不能同时用，以第一个为准
+//    public String say(@RequestParam(value = "id", required = false, defaultValue = "-1") Integer id) {
+//        return "id: " + id;
+//        return girlProperties.getCupSize();
+//        return "index";
+//    }
+
+
+}
